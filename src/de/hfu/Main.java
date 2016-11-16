cfc8f78c2ae5424cd0721d3026be700657d0c2eb
Christian Plotzky <christian@plotzky.net>
2016-11-16 17:20:43 +0100

    finished queries and parsing

--- src/de/hfu/Main.java
+++ src/de/hfu/Main.java
@@ -2,9 +2,15 @@ package de.hfu;
 
 import com.informix.jdbc.IfxConnection;
 import com.informix.jdbc.IfxDriver;
+import hfu.generator.Meta;
 import hfu.generator.WordInfo;
 import hfu.generator.WordInfos;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.Map;
 
 /**
  * Created by IMTT on 15.11.2016.
@@ -16,34 +22,25 @@ public class Main {
     private static String user;
     private static String password;
 
-    private static Connection connectIfx(){
-        try
-        {
+    private static Connection connectIfx() {
+        try {
             Class.forName("com.informix.jdbc.IfxDriver");
-            DriverManager.registerDriver((com.informix.jdbc.IfxDriver)Class.forName("com.informix.jdbc.IfxDriver").newInstance());
-        }
-        catch (Exception e)
-        {
+            DriverManager.registerDriver((com.informix.jdbc.IfxDriver) Class.forName("com.informix.jdbc.IfxDriver").newInstance());
+        } catch (Exception e) {
             System.out.println("ERROR: failed to load Informix JDBC driver.");
             e.printStackTrace();
             return null;
         }
-        //port 27017
         String database = "assignment04";
         String informixserver = "ol_informix1210";
-        String url = "jdbc:informix-sqli://" + ip +":" +port +"/"+database + ":INFORMIXSERVER="
+        String url = "jdbc:informix-sqli://" + ip + ":" + port + "/" + database + ":INFORMIXSERVER="
                 + informixserver
-        +";user=" +user +";password=" + password;
+                + ";user=" + user + ";password=" + password;
         System.out.println(url);
-//        System.out.println("User: " + user);
-//        System.out.println("Password: " + password);
-        try
-        {
-            Connection ifxConnection= DriverManager.getConnection(url, user, password);
+        try {
+            Connection ifxConnection = DriverManager.getConnection(url, user, password);
             return ifxConnection;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("ERROR: failed to connect!");
             System.out.println("ERROR: " + e.getMessage());
             e.printStackTrace();
@@ -52,9 +49,9 @@ public class Main {
     }
 
 
-    public static void main(String args[]) throws SQLException {
-        String path = "C:\\Users\\IMTT\\Downloads\\samples";
-        if (args.length>=3){
+    public static void main(String args[]) throws Exception {
+        String path = "D:\\Informationssysteme\\samples";
+        if (args.length >= 3) {
             path = args[0];
             ip = args[1];
             port = args[2];
@@ -63,12 +60,65 @@ public class Main {
         }
         Connection ifxConn = connectIfx();
         TableImpl ifxTable = new TableImpl(ifxConn);
-        for(WordInfo info : new WordInfos(path)) {
-            Date time = new Date(info.getMeta().getTime());
-            ifxTable.insertDocument(info.getMeta().getType(), info.getMeta().getUri(), new String(), time);
-            ifxTable.insertWord(info.getWord());
-            ifxTable.insertWordcount(info.getFrequency(), info.getMeta().getUri(), info.getWord());
+//        parseData(ifxTable, path);
+        long t0 = System.currentTimeMillis();
+        Map<Date, Integer> q1Res = ifxTable.query1("reddit", new Date(1353067200), new Date(System.currentTimeMillis()));
+        long t1 = System.currentTimeMillis();
+        System.out.println("Query 1: " + (t1 - t0) + " ms");
+        System.out.println("Showing 5 sample results...");
+        int i = 5;
+        for (Map.Entry<Date, Integer> entry :
+                q1Res.entrySet()) {
+            if (i > 0) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+                i--;
+            } else {
+                break;
+            }
+        }
+        long t2 = System.currentTimeMillis();
+        Map<String, Integer> q2Res = ifxTable.query2("reddit", new Date(1353067200), new Date(System.currentTimeMillis()), 10);
+        long t3 = System.currentTimeMillis();
+        System.out.println("Query 2: " + (t3 - t2) + " ms");
+        System.out.println("Showing 5 sample results...");
+        i = 5;
+        for (Map.Entry<String, Integer> entry :
+                q2Res.entrySet()) {
+            if (i > 0) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+                i--;
+            } else {
+                break;
+            }
         }
         ifxConn.close();
     }
+
+    public static void parseData(TableImpl ifxTable, String path) throws Exception {
+        for (WordInfo info : new WordInfos(path)) {
+            Meta meta = info.getMeta();
+            String url = meta.getUri();
+            String word = info.getWord();
+            int frequency = info.getFrequency();
+            String hash = convertToHash(url);
+            Date time = new Date(meta.getTime());
+            System.out.println("url: " + url + " hash: " + hash);
+            ifxTable.insertDocument(meta.getType(), url, new String(), time, hash);
+            ifxTable.insertWord(word);
+            ifxTable.insertWordcount(frequency, hash, word);
+        }
+    }
+
+    public static String convertToHash(String url) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(url.getBytes());
+        byte byteData[] = md.digest();
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 32).substring(1));
+        }
+        return sb.toString();
+    }
+
 }
--- src/de/hfu/TableImpl.java
+++ src/de/hfu/TableImpl.java
@@ -1,7 +1,5 @@
 package de.hfu;
 
-import com.ziclix.python.sql.connect.Connect;
-
 import java.sql.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -17,39 +15,78 @@ public class TableImpl {
     private PreparedStatement wordcountPs;
     private PreparedStatement wordPs;
     private PreparedStatement query1Ps;
-    private PreparedStatement query2Ps;
 
     public TableImpl(Connection connection) throws SQLException {
-        this.connection=connection;
-        this.documentPs = connection.prepareStatement("insert into documents (type, url, domain, time) values (?, ?, ?, ?)");
-        this.wordcountPs = connection.prepareStatement("insert into wordcounts (doc_id, word_id, count) select documents.id, words.id, ? from" +
-                "documents, words where documents.url=? and words.word=?");
-        this.wordPs = connection.prepareStatement("insert or ignore into words (word) values (?)");
-        this.query1Ps = connection.prepareStatement("");
-        this.query2Ps = connection.prepareStatement("");
+        this.connection = connection;
+        this.documentPs = connection.prepareStatement("insert into documents (type, url, domain, time, hash) values (?, ?, ?, ?, ?)");
+        this.wordcountPs = connection.prepareStatement("insert into wordcounts (doc_id, word_id, count) " +
+                "values((select id from documents where hash=?), (select id from words where word=?), ?)");
+        this.wordPs = connection.prepareStatement("insert into words (word) values (?)");
+        this.query1Ps = connection.prepareStatement("SELECT ds.time AS time, sum(wcs.count) as count " +
+                "FROM wordcounts AS wcs " +
+                "LEFT JOIN documents AS ds " +
+                "    ON ds.id = wcs.doc_id " +
+                "LEFT JOIN words AS ws " +
+                "    ON ws.id = wcs.word_id " +
+                "WHERE " +
+                "    ws.word = ? " +
+                "    AND " +
+                "    ds.time >= ? " +
+                "    AND " +
+                "    ds.time <= ? " +
+                "GROUP BY " +
+                "    ds.time " +
+                "; ");
     }
 
-    public void insertDocument(String type, String url, String domain, Date time) throws SQLException {
-        documentPs.setString(1, type);
-        documentPs.setString(2, url);
-        documentPs.setString(3, domain);
-        documentPs.setDate(4, time);
-        documentPs.executeUpdate();
+    public void insertDocument(String type, String url, String domain, Date time, String hash) throws SQLException {
+        try {
+            documentPs.setString(1, type);
+            documentPs.setString(2, url);
+            documentPs.setString(3, domain);
+            documentPs.setDate(4, time);
+            documentPs.setString(5, hash);
+            documentPs.executeUpdate();
+        } catch (SQLException e) {
+            //ignore duplicate keys
+            if (e.getErrorCode()!=-239){
+                throw e;
+            }
+        }
     }
 
-    public void insertWordcount(int count, String url, String word) throws SQLException {
-        wordcountPs.setInt(1, count);
-        wordcountPs.setString(2, url);
-        wordcountPs.setString(3, word);
-        wordcountPs.executeUpdate();
+    public void insertWordcount(int count, String hash, String word) throws SQLException {
+//        System.out.println("Insert count " + count + " for hash " + hash + " for word " + word);
+        try {
+            wordcountPs.setString(1, hash);
+            wordcountPs.setString(2, word);
+            wordcountPs.setInt(3, count);
+            wordcountPs.executeUpdate();
+        } catch (SQLException e) {
+            //ignore duplicate keys
+            if (e.getErrorCode()!=-239){
+                throw e;
+            }
+        }
     }
 
     public void insertWord(String word) throws SQLException {
-        wordPs.setString(1, word);
-        wordPs.executeUpdate();
+        try {
+            wordPs.setString(1, word);
+            wordPs.executeUpdate();
+        } catch (SQLException e) {
+            //ignore duplicate keys
+            if (e.getErrorCode()!=-239){
+                throw e;
+            }
+        }
+
     }
 
-    public Map<Date,Integer> query1(String word, String from, String to) throws SQLException {
+    public Map<Date, Integer> query1(String word, Date from, Date to) throws SQLException {
+        query1Ps.setString(1, word);
+        query1Ps.setDate(2, from);
+        query1Ps.setDate(3, to);
         ResultSet rs = query1Ps.executeQuery();
         Map<Date, Integer> dateCountmap = new HashMap<Date, Integer>();
         while (rs.next()) {
@@ -59,19 +96,39 @@ public class TableImpl {
 //                dateCountmap.put(time, dateCountmap.get(time)+count);
 //            }
 //            else{
-                dateCountmap.put(time, count);
+            dateCountmap.put(time, count);
 //            }
         }
         return dateCountmap;
     }
 
-    public LinkedHashMap<String,Integer> query2(String word, String from, String
+    public LinkedHashMap<String, Integer> query2(String word, Date from, Date
             to, int top) throws SQLException {
+        String query2String = "SELECT ds.url AS url, wcs.count AS count " +
+                "FROM wordcounts AS wcs " +
+                "LEFT JOIN documents AS ds " +
+                "   ON ds.id = wcs.doc_id " +
+                "LEFT JOIN words AS ws " +
+                "   ON ws.id = wcs.word_id " +
+                "WHERE " +
+                "   ws.word = ? " +
+                "   AND " +
+                "   ds.time >= ? " +
+                "   AND " +
+                "   ds.time <= ? " +
+                "ORDER BY " +
+                "   count DESC " +
+                "LIMIT "+ top + " " +
+                "; ";
+        PreparedStatement query2Ps = connection.prepareStatement(query2String);
+        query2Ps.setString(1, word);
+        query2Ps.setDate(2, from);
+        query2Ps.setDate(3, to);
         ResultSet rs = query2Ps.executeQuery();
         LinkedHashMap<String, Integer> wordCountOrdered = new LinkedHashMap<String, Integer>();
         while (rs.next()) {
             int count = rs.getInt("count");
-            String url = rs.getString("url");
+            String url = rs.getString("url").trim();
             wordCountOrdered.put(url, count);
         }
         return wordCountOrdered;
